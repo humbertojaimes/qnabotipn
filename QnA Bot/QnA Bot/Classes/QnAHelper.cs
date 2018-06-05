266cc2117dd276c499e8507eb6ce79f52bb4c91b
Humberto Jaimes <humberto.jaimes@outlook.com>
2018-06-05 13:04:02 -0500

    First Commit

---
+++ QnA Bot/QnA Bot/Classes/QnAHelper.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QnA_Bot.Classes
+{
+    public class QnAHelper
+    {
+        [JsonProperty(PropertyName = "answer")]
+        public string Answer { get; set; }
+
+        [JsonProperty(PropertyName = "score")]
+        public double Score { get; set; }
+
+        public static string HandleResponse(string receivedValue)
+        {
+            var response = JsonConvert.DeserializeObject<Rootobject>(receivedValue);
+            var selectedAnswer = response.answers[0].answer;
+            return selectedAnswer;
+        }
+    }
+
+    public class Rootobject
+    {
+        public Answer[] answers { get; set; }
+    }
+
+    public class Answer
+    {
+        public string answer { get; set; }
+        public string[] questions { get; set; }
+        public float score { get; set; }
+    }
+}
---
+++ QnA Bot/QnA Bot/Dialogs/RootDialog.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using QnA_Bot.Classes;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace QnA_Bot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as IMessageActivity;
+            string responseString = string.Empty;
+            var query = activity.Text.ToString();
+
+            string host = "";
+            string EndpointKey = "";
+            string knowledgeBase = ""; 
+
+            var builder = new UriBuilder($"{host}/qnamaker/knowledgebases/{knowledgeBase}/generateAnswer");
+            var postBody = $"{{\"question\": \"{query}\"}}";
+
+            using (WebClient client = new WebClient())
+            {
+                client.Encoding = System.Text.Encoding.UTF8;
+                client.Headers.Add("Authorization", EndpointKey);
+                client.Headers.Add("Content-Type", "application/json");
+                responseString = client.UploadString(builder.Uri, postBody);
+            }
+            var receivedAnswer = QnAHelper.HandleResponse(responseString);
+            await context.PostAsync(receivedAnswer);
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
